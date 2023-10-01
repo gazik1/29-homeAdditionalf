bebc2aad1b788ba272ea212ea6812d5ebf1d476b
gazik1 <112205916+gazik1@users.noreply.github.com>
2023-10-01 18:38:28 +0300

    29add

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function task1(){
-    let name=document.getElementById("task1")
-    let form=document.getElementById("task1_form")
-    form.button.onclick=()=>{
-        name.innerHTML=name.innerHTML+form.input.value
-    }
+    let form=document.getElementById("task1")
+    form.button.addEventListener("click",()=>{
+        console.log(form.text.value);
+        form.text.value=""
+    })
 }
 task1()
+
 function task2(){
-    let name=document.getElementById("task2")
-    let form=document.getElementById("task2_form")
-    form.button.onclick=()=>{
-        name.textContent=name.innerHTML+form.input.value
-    }
+    
+    let form=document.getElementById("task2-form")
+    form.button.addEventListener("click",()=>{
+        console.log(form.select.selectedOptions[0].textContent);
+    })
 }
 task2()
